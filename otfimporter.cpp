ca133d91f17293b261f7e8437d61ca7f3b1bffe8
Kate Isaacs <keisaacs@ucdavis.edu>
2013-11-04 12:13:18 -0800

    Added stubs for all the needed OTF handlers.

--- otfimporter.cpp
+++ otfimporter.cpp
@@ -38,15 +38,57 @@ void OTFImporter::readRawTrace()
 
 void OTFImporter::setHandlers()
 {
+    // Timer
     OTF_HandlerArray_setHandler(handlerArray,
                                 (OTF_FunctionPointer*) &OTFImporter::handleDefTimerResolution,
                                 OTF_DEFTIMERRESOLUTION_RECORD);
     OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_DEFTIMERRESOLUTION_RECORD);
 
+    // Function Names
     OTF_HandlerArray_setHandler(handlerArray,
                                 (OTF_FunctionPointer*) &OTFImporter::handleDefFunction,
                                 OTF_DEFFUNCTION_RECORD);
     OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_DEFFUNCTION_RECORD);
+
+    // Process Info
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleDefProcess,
+                                OTF_DEFPROCESS_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_DEFPROCESS_RECORD);
+
+    // Counter Names
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleDefCounter,
+                                OTF_DEFCOUNTER_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_DEFCOUNTER_RECORD);
+
+    // Enter & Leave
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleEnter,
+                                OTF_ENTER_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_ENTER_RECORD);
+
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleLeave,
+                                OTF_LEAVE_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_LEAVE_RECORD);
+
+    // Send & Receive
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleSend,
+                                OTF_SEND_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_SEND_RECORD);
+
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleRecv,
+                                OTF_RECEIVE_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_RECEIVE_RECORD);
+
+    // Counter Value
+    OTF_HandlerArray_setHandler(handlerArray,
+                                (OTF_FunctionPointer*) &OTFImporter::handleCounter,
+                                OTF_COUNTER_RECORD);
+    OTF_HandlerArray_setFirstHandlerArg(handlerArray, this, OTF_COUNTER_RECORD);
 }
 
 
@@ -67,5 +109,47 @@ int OTFImporter::handleDefFunction(void * userData, uint32_t stream, uint32_t fu
 
     std::cout << func << " : " << name << " - " << funcGroup << std::endl;
     (*((((OTFImporter*) userData)->rawtrace)->functions))[func] = QString(name);
+    return 0;
+}
+
+int OTFImporter::handleDefProcess(void * userData, uint32_t stream, uint32_t process,
+                            const char* name, uint32_t parent)
+{
+    return 0;
+}
+
+int OTFImporter::handleDefCounter(void * userData, uint32_t stream, uint32_t counter,
+                            const char* name, uint32_t properties, uint32_t counterGroup,
+                            const char* unit)
+{
+    return 0;
+}
+
+int OTFImporter::handleEnter(void * userData, uint64_t time, uint32_t function,
+                       uint32_t process, uint32_t source)
+{
+    return 0;
+}
+
+int OTFImporter::handleLeave(void * userData, uint64_t time, uint32_t function,
+                       uint32_t process, uint32_t source)
+{
+    return 0;
+}
+
+int OTFImporter::handleSend(void * userData, uint64_t time, uint32_t sender, uint32_t receiver,
+                      uint32_t group, uint32_t type, uint32_t length, uint32_t source)
+{
+    return 0;
+}
+int OTFImporter::handleRecv(void * userData, uint64_t time, uint32_t receiver, uint32_t sender,
+                      uint32_t group, uint32_t type, uint32_t length, uint32_t source)
+{
+    return 0;
+}
+
+int OTFImporter::handleCounter(void * userData, uint64_t time, uint32_t process, uint32_t counter,
+                         uint64_t value)
+{
     return 0;
 }
--- otfimporter.h
+++ otfimporter.h
@@ -14,6 +14,21 @@ public:
     static int handleDefTimerResolution(void * userData, uint32_t stream, uint64_t ticksPerSecond);
     static int handleDefFunction(void * userData, uint32_t stream, uint32_t func,
                                  const char* name, uint32_t funcGroup, uint32_t source);
+    static int handleDefProcess(void * userData, uint32_t stream, uint32_t process,
+                                const char* name, uint32_t parent);
+    static int handleDefCounter(void * userData, uint32_t stream, uint32_t counter,
+                                const char* name, uint32_t properties, uint32_t counterGroup,
+                                const char* unit);
+    static int handleEnter(void * userData, uint64_t time, uint32_t function,
+                           uint32_t process, uint32_t source);
+    static int handleLeave(void * userData, uint64_t time, uint32_t function,
+                           uint32_t process, uint32_t source);
+    static int handleSend(void * userData, uint64_t time, uint32_t sender, uint32_t receiver,
+                          uint32_t group, uint32_t type, uint32_t length, uint32_t source);
+    static int handleRecv(void * userData, uint64_t time, uint32_t receiver, uint32_t sender,
+                          uint32_t group, uint32_t type, uint32_t length;, uint32_t source);
+
+
 
     unsigned long long ticks_per_second;
 
