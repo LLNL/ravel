8f755df176a3c1c5f810e16ed6ca970ec2111307
Todd Gamblin <tgamblin@llnl.gov>
2015-02-11 15:12:32 -0800

    minor Cmake find module fixes.

--- CMake-Modules/FindMuster.cmake
+++ CMake-Modules/FindMuster.cmake
@@ -1,16 +1,15 @@
 find_path(Muster_INCLUDE_DIRS kmedoids.h
-    $ENV{HOME}/opt/include
-    $ENV{HOME}/opt/muster/include
+  PATHS $ENV{HOME}/opt/include $ENV{HOME}/opt/muster/include
+  PATH_SUFFIXES muster
 )
 
 find_library(Muster_LIBRARIES muster
-    $ENV{HOME}/opt/lib
-    $ENV{HOME}/opt/muster/lib
+  PATHS $ENV{HOME}/opt/lib $ENV{HOME}/opt/muster/lib
+  PATH_SUFFIXES muster
 )
 
 
 find_package_handle_standard_args(Muster
-	FAIL_MESSAGE "Couldn't find Muster."
-	REQUIRED_VARS Muster_INCLUDE_DIRS Muster_LIBRARIES
-	)
+  FAIL_MESSAGE "Couldn't find Muster."
+  REQUIRED_VARS Muster_INCLUDE_DIRS Muster_LIBRARIES)
 
--- CMake-Modules/FindOTF.cmake
+++ CMake-Modules/FindOTF.cmake
@@ -1,21 +1,21 @@
 find_path(OTF_INCLUDE_DIRS otf.h
-    $ENV{HOME}/opt/include
-    $ENV{HOME}/opt/include/otf
-    $ENV{HOME}/opt/include/open-trace-format
-)
+  PATHS $ENV{HOME}/opt/include $ENV{HOME}/opt/include/otf
+  PATH_SUFFIXES open-trace-format)
 
-find_library(OTF_LIBRARIES otf
-    $ENV{HOME}/opt/lib
-)
+find_library(
+  OTF_LIBRARIES otf
+  PATHS $ENV{HOME}/opt/lib
+  PATH_SUFFIXES open-trace-format)
 
 if (NOT OTF_LIBRARIES)
-    find_library(OTF_LIBRARIES open-trace-format
-	$ENV{HOME}/opt/lib
-    )
+  find_library(
+    OTF_LIBRARIES
+    PATHS $ENV{HOME}/opt/lib
+    PATH_SUFFIXES open-trace-format)
 endif()
 
-find_package_handle_standard_args(OTF
-	FAIL_MESSAGE "Couldn't find OTF library."
-	REQUIRED_VARS OTF_INCLUDE_DIRS OTF_LIBRARIES
-	)
+find_package_handle_standard_args(
+  OTF
+  FAIL_MESSAGE "Couldn't find OTF library."
+  REQUIRED_VARS OTF_INCLUDE_DIRS OTF_LIBRARIES)
 
