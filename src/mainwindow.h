ddfdbfb1dc7181b1113a6e4ed65683b581314a35
Kate Isaacs <keisaacs@ucdavis.edu>
2015-05-20 11:20:44 -0700

    Make View menu correspond to automatic on new trace settings.

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -77,7 +77,7 @@ signals:
 private:
     Ui::MainWindow *ui;
     void importTrace(QString dataFileName);
-    void activeTraceChanged();
+    void activeTraceChanged(bool first = false);
     void linkSideSplitter();
     void linkMainSplitter();
     void setVisWidgetState();
@@ -87,6 +87,7 @@ private:
     QVector<VisWidget *> viswidgets;
     QList<QAction *> visactions;
     QVector<int> splitterMap;
+    QVector<QAction *> splitterActions;
     int activeTrace;
 
     // For progress bar
@@ -107,6 +108,12 @@ private:
     QString activetracename;
 
     QStack<QString> activetraces;
+
+    static const int OVERVIEW = 0;
+    static const int STEPVIS = 1;
+    static const int TIMEVIS = 2;
+    static const int CLUSTERVIS = 3;
+    static const int CLUSTERTREEVIS = 4;
 };
 
 #endif // MAINWINDOW_H
