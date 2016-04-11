7fdab4f6454b620ab47065d49a3d7928df663998
Kate Isaacs <keisaacs@ucdavis.edu>
2016-04-11 09:24:40 -0700

    Remove excess layout on traditionalLabelWidget.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -113,7 +113,6 @@ MainWindow::MainWindow(QWidget *parent) :
                                                  visoptions);
     timevis->setClosed(true);
     ui->traditionalContainer->layout()->addWidget(timevis);
-    ui->traditionalLabelWidget->setLayout(new QVBoxLayout());
     ui->traditionalLabelWidget->layout()->addWidget(new VerticalLabel("Physical",
                                                                       ui->traditionalLabelWidget));
     QLabel * timescaleLabel = new QLabel(ui->traditionalLabelWidget);
