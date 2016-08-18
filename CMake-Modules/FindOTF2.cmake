8e33f5e4412fa7f0a52df94f3c023c96f49dda71
Kate Isaacs <keisaacs@ucdavis.edu>
2016-08-17 17:12:09 -0700

    Add OTF2 default dir to FindOTF2.

--- CMake-Modules/FindOTF2.cmake
+++ CMake-Modules/FindOTF2.cmake
@@ -1,9 +1,11 @@
 find_path(OTF2_INCLUDE_DIRS otf2/otf2.h
     $ENV{HOME}/opt/include
+    /usr/opt/otf2/include
 )
 
 find_library(OTF2_LIBRARIES otf2
     $ENV{HOME}/opt/lib
+    /usr/opt/otf2/lib
 )
 
 
