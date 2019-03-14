24d66f9d810fc6fd4c8ca25311755efd475b0536
Xavier Lacoste <xavier.lacoste@total.com>
2019-03-14 14:17:01 +0100

    min/max requires arguments of the same kind

--- src/stepvis.cpp
+++ src/stepvis.cpp
@@ -1448,9 +1448,9 @@ void StepVis::drawPrimaryLabels(QPainter * painter, int effectiveHeight,
         skip = ceil(float(entitySpan) / total_labels);
     }
 
-    int start = std::max(floor(startEntity), 0.0);
+    int start = std::max(floor(startEntity), 0.0f);
     int end = std::min(ceil(startEntity + entitySpan),
-                       maxEntities - 1.0);
+                       maxEntities - 1.0f);
 
     int current = start;
     int offset = 0;
